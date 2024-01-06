359f070d1e305674f5c9c4e0828f52fba3fcdff4
Shiaba <>
2024-01-06 22:26:12 +0000

    Add basic navbar components & favicon image

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import NavBar from "./components/NavBar";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Test</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <NavBar />
+        </div>
+    );
 }
 
 export default App;
