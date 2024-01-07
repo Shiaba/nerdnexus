6a50b70906ebafc37340d760122f4052e44c2468
Shiaba <>
2024-01-07 19:02:56 +0000

    Install router & add css & link to the NavLink

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -2,20 +2,35 @@ import React from 'react';
 import { Navbar, Container, Nav} from "react-bootstrap";
 import logo from '../assets/logo.png';
 import styles from '../styles/NavBar.module.css';
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
     return (
         <Navbar className={styles.NavBar} expand="md" fixed="top">
             <Container>
+                <NavLink to="/">
                 <Navbar.Brand>
                     <img src={logo} alt="logo" height="60" />
-                </Navbar.Brand>
+                    </Navbar.Brand>
+                </NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto text-left">
-                        <Nav.Link><i className='fa-solid fa-house'></i>Home</Nav.Link>
-                        <Nav.Link><i className="fa-solid fa-right-to-bracket"></i>Sign in</Nav.Link>
-                        <Nav.Link><i className="fa-solid fa-user-plus"></i>Sign up</Nav.Link>
+                        <NavLink exact className={styles.NavLink} 
+                            activeClassName={styles.Active} 
+                            to="/">
+                            <i className='fa-solid fa-house'></i>Home
+                        </NavLink>
+                        <NavLink className={styles.NavLink} 
+                            activeClassName={styles.Active} 
+                            to="/signin">
+                            <i className="fa-solid fa-right-to-bracket"></i>Sign in
+                        </NavLink>
+                        <NavLink className={styles.NavLink} 
+                            activeClassName={styles.Active} 
+                            to="/signup">
+                            <i className="fa-solid fa-user-plus"></i>Sign up
+                        </NavLink>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
