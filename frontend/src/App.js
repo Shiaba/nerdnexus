190d67c3ff3c7adb68e7719e3ede385a4b1fe304
Shiaba <>
2024-01-08 19:13:16 +0000

    reinstall axios & react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,12 @@ import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container";
 import { Route, Switch } from 'react-router-dom';
+import './api/axiosDefaults';
 import SignUpForm from './pages/auth/SignUpForm';
 import SignInForm from './pages/auth/SignInForm';
 import { createContext, useEffect, useState } from 'react';
 import axios from 'axios';
+import './api/axiosDefaults';
 
 export const CurrentUserContext = createContext();
 export const SetCurrentUserContext = createContext();
