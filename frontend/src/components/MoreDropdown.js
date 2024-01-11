78f51ecfe2c07c8d42c7e01badca5f96937c2134
Siavash Bahrami <siavash.bahrami@hotmail.com>
2024-01-11 19:32:04 +0000

    Add dropdown menu for edit & delete to posts

---
+++ frontend/src/components/MoreDropdown.js
@@ -0,0 +1,45 @@
+import React from "react";
+import Dropdown from "react-bootstrap/Dropdown";
+import styles from "../styles/MoreDropdown.module.css";
+
+
+// dropdown for posts edit & delete
+
+const ThreeDots = React.forwardRef(({ onClick }, ref) => (
+    <i
+        className="fa-solid fa-ellipsis-vertical"
+        ref={ref}
+        onClick={(e) => {
+            e.preventDefault();
+            onClick(e);
+        }}
+    />
+));
+
+export const MoreDropdown = ({ handleEdit, handleDelete }) => {
+    return (
+        <Dropdown className="ml-auto" drop="left">
+            <Dropdown.Toggle as={ThreeDots} />
+
+            <Dropdown.Menu
+                className="text-center"
+                popperConfig={{ strategy: "fixed" }}
+            >
+                <Dropdown.Item
+                    className={styles.DropdownItem}
+                    onClick={handleEdit}
+                    aria-label="edit"
+                >
+                    <i className="fa-regular fa-pen-to-square" />
+                </Dropdown.Item>
+                <Dropdown.Item
+                    className={styles.DropdownItem}
+                    onClick={handleDelete}
+                    aria-label="delete"
+                >
+                    <i className="fa-solid fa-trash" />
+                </Dropdown.Item>
+            </Dropdown.Menu>
+        </Dropdown>
+    );
+};
