42b63fa44fe0afc61443db974dce1122a1719f72
Siavash Bahrami <siavash.bahrami@hotmail.com>
2024-01-17 10:53:48 +0000

    Add settings for front & backend-end deployment

--- frontend/src/api/axiosDefaults.js
+++ frontend/src/api/axiosDefaults.js
@@ -1,15 +1,8 @@
 import axios from "axios";
 
-// axios.defaults.baseURL = "";
+axios.defaults.baseURL = "/api";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
 export const axiosReq = axios.create();
 export const axiosRes = axios.create();
-
-// IMPORTANT!!
- // Because this React app is running in the same workspace as the API,
-
- // there is no need to set a separate baseURL until you reach deployment.
-
- // Setting a baseURL before you reach deployment will cause errors
